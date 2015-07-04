9b6aaf64294dbba9e6443affe038019c5264a1d2
Abílio Oliveira <eco.abilio@gmail.com>
2015-07-04 00:19:02 -0300

    Teste com model2 funcionando de acordo com o esperado.

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,6 +9,8 @@ import FeatureFamilyBasedAnalysisTool.FDTMC;
 import Modeling.DiagramAPI;
 import Modeling.ActivityDiagrams.ADUtil;
 import Modeling.Exceptions.InvalidGuardException;
+import Modeling.Exceptions.InvalidNodeClassException;
+import Modeling.Exceptions.InvalidNumberOfOperandsException;
 import Modeling.Exceptions.InvalidTagException;
 import Modeling.Exceptions.UnsupportedFragmentTypeException;
 import Modeling.SequenceDiagrams.SDReader;
@@ -17,8 +19,8 @@ import Modeling.SequenceDiagrams.SDUtil;
 public class Main {
 	private static HashMap<String, FDTMC> fdtmcByName;
 	
-	public static void main(String[] args) throws InvalidTagException, UnsupportedFragmentTypeException, DOMException, InvalidGuardException {
-		File xmlFile = new File("model.xml");
+	public static void main(String[] args) throws InvalidTagException, UnsupportedFragmentTypeException, DOMException, InvalidGuardException, InvalidNumberOfOperandsException, InvalidNodeClassException {
+		File xmlFile = new File("model2.xml");
 		
 		DiagramAPI diagram = new DiagramAPI(xmlFile);
 		diagram.initialize();
@@ -26,7 +28,7 @@ public class Main {
 		for (SDReader sdp : diagram.getSdParsers()) {
 			SDUtil.printAll(sdp);
 		}
-//		diagram.transform();
+		diagram.transform();
 		
 		fdtmcByName = diagram.getFdtmcByName();
 	}
