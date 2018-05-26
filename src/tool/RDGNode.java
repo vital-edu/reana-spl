13b13a786167b4033703581aec49daed609066cc
Eduardo Vital Alencar Cunha <vitaldu@gmail.com>
2018-05-26 01:02:02 +0000

    Using extract method in RDGNode
    Signed-off-by: Eduardo Vital Alencar Cunha <vitaldu@gmail.com>
    Signed-off-by: Leonardo Cambraia <cambraia@protonmail.com>

--- src/tool/RDGNode.java
+++ src/tool/RDGNode.java
@@ -228,11 +228,19 @@ public class RDGNode {
      */
     public static RDGNode getSimilarNode(RDGNode target) {
         for (RDGNode candidate: nodesInCreationOrder) {
-            if (candidate != target && candidate.equals(target)) {
+            if (candidate.isNotTargetAndEquals(target)) {
                 return candidate;
             }
         }
         return null;
+    }
+    
+    private boolean isNotTargetAndEquals(RDGNode target){
+    	if(this != target && this.equals(target)){
+    		return true;
+    	} else {
+    		return false;
+    	}
     }
 
     /**
